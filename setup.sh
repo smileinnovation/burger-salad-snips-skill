3e8f85bb03ab472e904c9959d29c129567c804f2
Sébastien Garcin-Beldowski <yenshu@pm.me>
2019-01-10 13:40:59 +0100

    Added installations setups

--- setup.sh
+++ setup.sh
@@ -2,5 +2,9 @@
 
 mkdir -p venv/bin/
 ln -s /opt/movidius/virtualenv-python/bin/activate venv/bin/activate
-#pip install AWSIoTPythonSDK --user
-#pip install hermes-python --user
+. venv/bin/activate
+pip install AWSIoTPythonSDK --user
+pip install hermes-python --user
+pip install opencv-python==3.3.0.10 --user
+deactivate
+
