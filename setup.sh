7e2647594660a62e3c24c0352eb1b20fecb79f6d
Sébastien Garcin-Beldowski <yenshu@pm.me>
2019-01-10 10:07:22 +0100

    Changed install instructions

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #/usr/bin/env bash -e
 
-source /opt/movidius/virtualenv-python/bin/activate
+. /opt/movidius/virtualenv-python/bin/activate
 pip install AWSIoTPythonSDK --user
-pip install hermes-python
+pip install hermes-python --user
