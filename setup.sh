806fdddbfeb3b04c269aec823e04c679fd5b4528
Sébastien Garcin-Beldowski <yenshu@pm.me>
2019-01-14 17:59:05 +0100

    Added video rights to snips

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #/usr/bin/env bash -e
 
-sudo usermod -a -G video $(whoami)
+sudo chmod 777 /dev/vchiq
 mkdir -p venv/bin/
 ln -s /opt/movidius/virtualenv-python/bin/activate venv/bin/activate
 . venv/bin/activate
