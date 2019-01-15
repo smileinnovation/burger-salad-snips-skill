2c3ac26d34d6f869b76eb0eb561e8bf5bb0c6bcc
Sébastien Garcin-Beldowski <19351477+Yenshu@users.noreply.github.com>
2019-01-15 10:21:01 +0100

    Removed un necessary steps

--- setup.sh
+++ setup.sh
@@ -1,10 +1,6 @@
 #/usr/bin/env bash -e
 
-sudo chmod 777 /dev/vchiq
 mkdir -p venv/bin/
 ln -s /opt/movidius/virtualenv-python/bin/activate venv/bin/activate
 . venv/bin/activate
-echo "installing dependencies"
 sudo pip3 install -r requirements
-echo "done"
-deactivate
