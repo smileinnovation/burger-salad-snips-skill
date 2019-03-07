5f9d3d83820415c5d6b57eebc0a46052afc182b1
Yenshu <yenshu@pm.me>
2019-03-07 15:35:36 +0100

    Aded matrix package

--- manual-setup.sh
+++ manual-setup.sh
@@ -5,6 +5,12 @@ curl -k https://apt.matrix.one/doc/apt-key.gpg | sudo apt-key add -
 echo "deb https://apt.matrix.one/raspbian $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/matrixlabs.list
 sudo apt-get update
 
+#Checking if all the packages needed are installed
+sudo apt-get install --yes python3-pip matrixio-malos gnome-schedule matrixio-kernel-modules
+
+#Setting config file for Matrix & Snips
+sudo sed -i 's/# mike = "Built-in Microphone"/mike = "MATRIXIO SOUND: - (hw:2,0)"/g' /etc/snips.toml
+
 #Adding cron task
 #write out current crontab
 crontab -l > mycron
@@ -13,12 +19,6 @@ echo "@reboot python /var/lib/snips/skills/burger-salad-snips-skill/utils/audio.
 #install new cron file
 crontab mycron
 rm mycron
-
-#Checking if all the packages needed are installed
-sudo apt-get install --yes python3-pip matrixio-malos gnome-schedule
-
-#Setting config file for Matrix & Snips
-sudo sed -i 's/# mike = "Built-in Microphone"/mike = "MATRIXIO SOUND: - (hw:2,0)"/g' /etc/snips.toml
 
 #Giving snips access to USB ports and video feed.
 sudo usermod -a -G video _snips-skills
